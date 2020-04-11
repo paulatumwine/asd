79fbf5ad865b539adb52e0e83cd4f21c196fb55f
Paul A. Tumwine <paulatumwine@gmail.com>
2020-04-11 16:50:19 -0500

    Track demos using singleton pattern
    Aha! Double check locking pattern - don't synchronize the whole method, only the instantiation

---
+++ singletonpattern/src/main/java/serializationsafe/SingletonTest.java
@@ -0,0 +1,25 @@
+package serializationsafe;
+
+import java.io.*;
+
+public class SingletonTest {
+	public static void main(String[] args) {
+		try {
+			ConnectionPool instance1 = ConnectionPool.getPool();
+			ObjectOutput out = new ObjectOutputStream(new FileOutputStream("filename.ser"));
+			out.writeObject(instance1);
+			out.close();
+
+			// deserialize from file to object
+			ObjectInput in = new ObjectInputStream(new FileInputStream("filename.ser"));
+			ConnectionPool instance2 = (ConnectionPool) in.readObject();
+			in.close();
+
+			System.out.println("instance1 hashCode=" + instance1.hashCode());
+			System.out.println("instance2 hashCode=" + instance2.hashCode());
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
