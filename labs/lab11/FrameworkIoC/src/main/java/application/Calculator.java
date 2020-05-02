75aa8ca830c229f9f4377d40d0a7aa687540757a
Paul A. Tumwine <paulatumwine@gmail.com>
2020-05-01 22:10:28 -0500

    Add and try an equals assertion

---
+++ labs/lab11/FrameworkIoC/src/main/java/application/Calculator.java
@@ -0,0 +1,9 @@
+package application;
+
+public interface Calculator {
+    void reset();
+
+    int add(int newValue);
+
+    int subtract(int newValue);
+}
--- labs/lab11/FrameworkIoC/src/main/java/application/MyTest.java
+++ labs/lab11/FrameworkIoC/src/main/java/application/MyTest.java
@@ -1,19 +1,30 @@
 package application;
 
+import framework.Before;
 import framework.Test;
 import framework.TestClass;
 
+import static framework.Asserts.assertEquals;
+
 @TestClass
 public class MyTest {
-	
-	@Test
-	public void testMethod1() {
-		System.out.println("perform test method 1");
-	}
+    Calculator calculator;
 
-	@Test
-	public void testMethod2() {
-		System.out.println("perform test method 2");
-	}
+    @Before
+    public void init() {
+        calculator = new CalculatorImpl();
+    }
+
+    @Test
+    public void testMethod1() {
+        assertEquals(calculator.add(3), 3);
+        assertEquals(calculator.add(6), 9);
+    }
+
+    @Test
+    public void testMethod2() {
+        assertEquals(calculator.add(3), 3);
+        assertEquals(calculator.subtract(6), -1);
+    }
 
 }
